6604c8f0eb94bcba972ee912542152a25c35c330
mai0809 <Maitrayee.Kar@fnf.com>
2021-09-06 15:10:52 +0530

    Add project files.

---
+++ ConsoleApp2/Program 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the amount of cel:");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("kelvin={0}", celsius + 273);
+            Console.WriteLine("Farenheit={0}", celsius *18/10+32);
+
+        }
+    }
+}
